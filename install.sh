a303107fbecbebc12d330c705f51d3d14f49cbf0
jyp220 <jyp@hatiolab.com>
2020-04-20 17:03:47 +0900

    test

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ HOST_PORT=3000
 echo "${HOST_PORT}"
 if [ $# -eq 0 ] ; then
   echo "Warning: default port 3000"
-  HOST_PORT="3000"
 else
-  HOST_PORT=$1
+  HOST_PORT=$0
 fi
+echo $@
 echo "${HOST_PORT}"
 
 echo "HostPort="$HOST_PORT > .env
