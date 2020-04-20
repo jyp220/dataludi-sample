81d07a87b7d286216f28ed95049942494db29d22
jyp220 <jyp@hatiolab.com>
2020-04-20 15:15:56 +0900

    test

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 
-curl -O https://github.com/jyp220/dataludi-sample/raw/master/db.sqlite
-curl -O https://github.com/jyp220/dataludi-sample/raw/master/docker-compose.yml
+curl -O https://raw.githubusercontent.com/jyp220/dataludi-sample/master/db.sqlite
+curl -O https://raw.githubusercontent.com/jyp220/dataludi-sample/master/docker-compose.yml
 
 # echo "start"
 # echo $1
