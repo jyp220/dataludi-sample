2d3e57f6ea16384188512dd4b7be18f846a3f160
jyp220 <jyp@hatiolab.com>
2020-04-20 14:36:22 +0900

    test

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
+
 curl -O https://github.com/jyp220/dataludi-sample/blob/master/db.sqlite
 curl -O https://github.com/jyp220/dataludi-sample/blob/master/docker-compose.yml
 
+# echo "start"
+# echo $1
+
+
 docker-compose up
+
+# echo "end"
