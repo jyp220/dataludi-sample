e31ad94cdc30dda9c0dc95fb7df32aa23118e028
jyp220 <jyp@hatiolab.com>
2020-04-20 16:49:01 +0900

    test

--- install.sh
+++ install.sh
@@ -5,7 +5,18 @@ curl -O https://raw.githubusercontent.com/jyp220/dataludi-sample/master/docker-c
 # echo "start"
 # echo $1
 
-echo "HostPort="$1 > .env
+HOST_PORT=3000
+
+echo "${HOST_PORT}"
+if [ $# -eq 0 ] ; then
+  echo "Warning: default port 3000"
+  HOST_PORT="3000"
+else
+  HOST_PORT=$1
+fi
+echo "${HOST_PORT}"
+
+echo "HostPort="$HOST_PORT > .env
 
 
 docker-compose up
